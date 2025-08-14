ebfaab27c3ebfe880b015382f67e3a0af689934e
Clint <hello@clintonryan.com>
2025-08-15 00:11:58 +1000

    Updates to on content loaded

--- assets/org/theme.js
+++ assets/org/theme.js
@@ -1,4 +1,4 @@
-(function () {
+document.addEventListener("DOMContentLoaded", function () {
   const toggleButton = document.createElement("button");
   toggleButton.textContent = "🌙 Dark mode";
   toggleButton.className = "theme-toggle";
@@ -21,4 +21,4 @@
     toggleButton.textContent =
       newTheme === "dark" ? "☀️ Light mode" : "🌙 Dark mode";
   });
-})();
+});
